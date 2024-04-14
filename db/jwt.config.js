2b787ce652ad2fbfae434692d9527527c08fb87b
Piyush Kumar <kumarchspiyush@gmail.com>
2024-04-15 01:46:58 +0530

    jbsjndfj

--- db/jwt.config.js
+++ db/jwt.config.js
@@ -4,7 +4,7 @@ module.exports.authenticate = function(req, res, next){
     try {
         const token = req.cookies.usertoken;
         console.log("Token received:", token);
-        const decoded = jwt.verify(token, "abcdef");
+        const decoded = jwt.verify(token, "1123326285sfgdgvx");
         req.userId = decoded.id;
         next();
     } catch(e) {
